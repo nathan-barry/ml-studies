4617ea25c374ffb274ce6a20a5a69566faa86486
Nathan Barry <nathanbarry474@gmail.com>
2023-01-06 13:00:00 +0100

    add title

---
+++ nnfs/p7.py
@@ -0,0 +1,14 @@
+# Calculating Loss with Categorical Cross-Entropy
+
+import math
+
+softmax_output = [0.7, 0.1, 0.2]
+target_output = [1, 0, 0]
+
+loss = -(math.log(softmax_output[0])*target_output[0] +
+         math.log(softmax_output[1])*target_output[1] +
+         math.log(softmax_output[2])*target_output[2])
+print(loss)
+
+loss = -(math.log(softmax_output[0]))
+print(loss)
